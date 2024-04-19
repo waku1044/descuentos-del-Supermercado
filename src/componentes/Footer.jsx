a2e17a1cbee9eebb0e83dc399f4dffd843ef64f5
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-04-19 19:22:03 -0300

    Update Footer.jsx

--- src/componentes/Footer.jsx
+++ src/componentes/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = ()=>{
         <>
             <div className="flex justify-center items-center flex-col bg-slate-900 py-5">
                 <h4 className="text-md text-amber-100  py-3 px-5 rounded-3xl ">Todos los derechos reservados. </h4>
-                <p className="text-md text-amber-100 te-800 py-3 px-5 rounded-3xl ">Diseñado por Destino Web | 2024 | Argentina.</p>
+                <p className="text-md text-amber-100 text-center te-800 py-3 px-5 rounded-3xl ">Diseñado por Destino Web | 2024 | Argentina.</p>
             </div>
         </>
     )
