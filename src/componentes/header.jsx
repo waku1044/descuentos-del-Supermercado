e66f05e27c9f08d3081c8e1736d32bdb5f06700a
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-04-20 04:25:20 -0300

    probar aplicacion

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -7,7 +7,7 @@ const header = () => {
         <>
             <header className="header flex  items-center">
             {/* <img src={logo} className='w-20' alt="Logo" /> */}
-                <h1 className="text-3xl font-bold  text-center ps-10">Descuentos en el Super</h1>
+                <h1 className="text-3xl font-bold text-slate-800  text-center ps-10">Descuentos en el Super</h1>
                 {/* <img src={logo} className='w-20' alt="Logo" /> */}
             </header>
         </>
--- src/componentes/inputs.jsx
+++ src/componentes/inputs.jsx
@@ -1,65 +1,120 @@
 import { useState } from "react";
-import '../assets/css/inputs.css'
+import "../assets/css/inputs.css";
 
 const Inputs = () => {
   const [hidden, setHidden] = useState(true);
-  const [porcentaje, setPorcentaje] = useState('');
-  const [precio, setPrecio] = useState('');
-  const [error, setError] = useState(false);
+  const [aplica, setAplica] = useState("trespordos");
+  const [porcentaje, setPorcentaje] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [error, setError] = useState({});
+  const [mostrar, setMostrar] = useState("");
 
   function verificar() {
-    if (porcentaje === '' || precio === '') {
-      setError("Por favor rellene todos los campos");
-    }if(porcentaje < 0 || precio < 0){
-      setError("No pueden ser negativos");
+    let errorList = {};
+    if(hidden){
+      if (precio === "") {
+        errorList = { ...errorList, precio: "Por favor rellene el campo" };
+      }
+      if (precio < 0) {
+        errorList = { ...errorList, precio: "No pueden ser negativos" };
+      }
+
+    }else{
+      if (precio === "") {
+        errorList = { ...errorList, precio: "Por favor rellene el campo" };
+      }
+      if (precio < 0) {
+        errorList = { ...errorList, precio: "No pueden ser negativos" };
+      }
+      if (porcentaje === "") {
+        errorList = { ...errorList, porcentaje: "Por favor rellene el campo" };
+      }
+      if (porcentaje < 0) {
+        errorList = { ...errorList, porcentaje: "No pueden ser negativos" };
+      }
+
     }
+    return errorList;
   }
-  function sinInputPorcentaje(number) {
+  function sinInputPorcentaje() {
     var debePagar = 0;
     var msj = "";
-    switch (porcentaje) {
+    console.log(porcentaje);
+    console.log(precio);
+    console.log(aplica)
+    switch (aplica) {
       case "trespordos":
-        debePagar = (number * 3) / 2;
-        msj =  `<h2>El total a pagar es: ${debePagar}</h2>
-        <p>Estaria llevando 3 productos al precio de ${debePagar / 3} cada uno</p>
+        debePagar = precio * 2 ;
+        msj = `El total a pagar es: $${debePagar}.
+        \n
+        Estarias llevando 3 productos al precio de $${
+          (debePagar / 3).toFixed(2)
+        } cada uno.
         `;
         break;
-      case "ochenta":
-        debePagar =  (number * 80) / 100;
-        msj =  `<h2>El total a pagar es: ${debePagar}</h2>
-        <p>Estaria llevando 2 productos al precio de ${debePagar / 2} cada uno</p>`; 
+        case "ochenta":
+        var des = precio * 0.8;
+        debePagar = precio * 2 - des;
+        msj = `El total a pagar es: $${debePagar}.\n
+        Estarias llevando 2 productos al precio de $${
+          debePagar / 2
+        } cada uno.`;
         break;
       case "setenta":
-        debePagar =  (number * 70) / 100;
-        msj =  `<h2>El total a pagar es: ${debePagar}</h2>
-        <p>Estaria llevando 2 productos al precio de ${debePagar / 2} cada uno</p>`; 
+        des = precio * 0.7;
+        debePagar = precio * 2 - des;
+        msj = `El total a pagar es: $${debePagar}.\n
+        Estarias llevando 2 productos al precio de $${
+          debePagar / 2
+        } cada uno.`;
 
         break;
       case "cincuenta":
-        debePagar =  (number * 50) / 100;
-        msj =  `<h2>El total a pagar es: ${debePagar}</h2>
-        <p>Estaria llevando 2 productos al precio de ${debePagar / 2} cada uno</p>`; 
+        des = precio * 0.5;
+        debePagar = precio * 2 - des;
+        msj = `El total a pagar es: $${debePagar}.
+        Estarias llevando 2 productos al precio de $${
+          debePagar / 2
+        } cada uno.`;
         break;
       case "descuento":
-        debePagar =  (number * porcentaje) / 100;
-        msj =  `<h2>El total a pagar es: ${debePagar}</h2>`;
+        debePagar = (precio * porcentaje) / 100;
+        msj = `El total a pagar es: $${debePagar}.`;
         break;
       default:
-        msj = '<h2>Surgio un erro ren la aplicacion</h2>';
+        msj = "Surgio un error en la aplicacion.";
     }
-    return msj;
+    setMostrar(msj);
+    console.log(msj);
   }
   function evaluar(opcion) {
     if (opcion === "descuento") {
       setHidden(false);
-    }else{
+    } else {
       setHidden(true);
+    }
+
+    setAplica(opcion);
+  }
+
+  function handleSend(e) {
+    e.preventDefault();
+    setError({});
+    let response = verificar();
+    
+    if (Object.keys(response).length < 1) {
+      sinInputPorcentaje();
+      
+    } else {
+      setError(response);
     }
   }
   return (
     <>
       <div className="fondo flex justify-center items-center flex-col bg-slate-900 py-12">
-        <h4 className="text-md lg:text-2xl text-amber-100 bg-slate-800 py-3 px-5 rounded-3xl ">Seleccione el descuento que desea aplicar</h4>
+        <h4 className="text-md lg:text-2xl text-amber-100 bg-slate-800 py-3 px-5 rounded-3xl ">
+          Seleccione el descuento que desea aplicar
+        </h4>
         <select
           className="w-1/2 border-2 border-gray-300 rounded-md p-2 m-2"
           onChange={(e) => evaluar(e.target.value)}
@@ -78,8 +133,10 @@ const Inputs = () => {
           value={porcentaje}
           onChange={(e) => setPorcentaje(e.target.value)}
         />
-        <p>{error && error} </p>
-        <h4 className="text-md lg:text-2xl text-amber-100 bg-slate-800 py-4 px-5 rounded-3xl ">Ingrese el precio del producto</h4>
+        {error.porcentaje && <p className="text-black  bg-red-500 px-5 py-1 rounded-3xl">{error.porcentaje}</p>}
+        <h4 className="text-md lg:text-2xl text-amber-100 bg-slate-800 py-4 px-5 rounded-3xl ">
+          Ingrese el precio del producto
+        </h4>
         <input
           type="number"
           placeholder="Ingrese el precio del producto"
@@ -87,11 +144,16 @@ const Inputs = () => {
           value={precio}
           onChange={(e) => setPrecio(e.target.value)}
         />
-        <button className="w-1/4  text-slate-900 font-bold tracking-wide  rounded-md p-2 m-2 bg-green-500 hover:bg-green-700 hover:text-gray-200">
+        {error.precio && <p className="text-black  bg-red-500 px-5 py-1 rounded-3xl">{error.precio}</p>}
+        <button
+          className="w-1/4  text-slate-900 font-bold tracking-wide  rounded-md p-2 m-2 bg-green-500 hover:bg-green-700 hover:text-gray-200"
+          onClick={handleSend}
+        >
           Calcular
         </button>
-      </div>
 
+        {mostrar && (<h2 className="text-md lg:text-2xl text-amber-100 bg-slate-800 py-4 px-5 rounded-3xl ">{mostrar}</h2>)}
+      </div>
     </>
   );
 };
