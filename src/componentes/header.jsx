40d762ce8643b3246e7461c9bfb195950541e013
Walter Gimenez <75543829+waku1044@users.noreply.github.com>
2024-04-20 05:11:59 -0300

    Update header.jsx

--- src/componentes/header.jsx
+++ src/componentes/header.jsx
@@ -5,7 +5,7 @@ const header = () => {
 
     return (
         <>
-            <header className="header flex items-center">
+            <header className="header flex justify-center items-center">
             {/* <img src={logo} className='w-20' alt="Logo" /> */}
                 <h1 className="text-3xl font-bold text-slate-800  text-center">Descuentos en el Super</h1>
                 {/* <img src={logo} className='w-20' alt="Logo" /> */}
